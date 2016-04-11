58c76922c7185159e177cb67d4b9b5ebeba49e6b
yunxiaozou <dfs>
2016-04-11 09:18:10 -0400

    lkl

--- 1-1/src/BinarySearch.java
+++ 1-1/src/BinarySearch.java
@@ -1,3 +1,9 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
 /**
  * Created by yunxiaozou on 3/27/16.
  */
@@ -15,5 +21,17 @@ public class BinarySearch {
     }
     public static boolean exists(int key, int[] a) {
         return rank(key, a) < 0;
+    }
+
+    public static void main(String[] args) {
+
+        int[] whitelist = In.readInts(args[0]);
+        int[] numbers = In.readInts(args[1]);
+        Arrays.sort(whitelist);
+        for (int num : numbers) {
+            if (rank(num, numbers) < 0)
+                System.out.println(num);
+        }
+
     }
 }
